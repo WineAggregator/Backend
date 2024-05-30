c130408186a1e5961116e300335e4f13efcada48
Catsher <iluha_m_74@mail.ru>
2024-05-30 20:15:14 +0500

    Сделал Сваггер публичным

--- Utils/MiddlewareConfiguring.cs
+++ Utils/MiddlewareConfiguring.cs
@@ -4,11 +4,8 @@ public static class MiddlewareConfiguring
 {
     public static void RegisterAllMiddlewares(this WebApplication app)
     {
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+        app.UseSwagger();
+        app.UseSwaggerUI();
 
         app.UseHttpsRedirection();
         app.UseAuthorization();
